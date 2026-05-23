98e623834bf7ec6cea2535b93e65d92d3ac69d75
DavidPach02 <davidjustin.pacheco@gmail.com>
2026-05-23 22:04:12 +0800

    Updated main by reprinting the header if the screen is cleared

--- CSC511C-MP/CSC511C-MP.cpp
+++ CSC511C-MP/CSC511C-MP.cpp
@@ -5,8 +5,36 @@
 #include "CommandDispatcher.h"
 #include "ICommand.h"
 
-int main()
-{
+void PrintHeader();
+
+int main(){
+    // Create command dispatcher object
+    CommandDispatcher dispatcher;
+	// Initialize the dispatcher with available commands
+	CommandDispatcher::Initialize(dispatcher);
+
+	PrintHeader();
+
+    bool running = true;
+	// Main command loop
+    while (running) {
+		// Prompt the user for input
+        std::cout << "Enter a command: ";
+        std::string command;
+        std::getline(std::cin, command);
+
+		// Dispatch the command and update the running state based on the command's return value
+		running = dispatcher.DispatchCommand(command);
+
+        if(command == "clear") {
+			PrintHeader();
+		}
+    }
+
+    return 0;
+}
+
+void PrintHeader() {
     std::cout << "\n";
     std::cout << "====================================================================================\n\n";
     std::cout << "  ,ad8888ba,    ad88888ba     ,ad8888ba,   8888888888    88      88    ,ad8888ba,   \n";
@@ -20,24 +48,4 @@ int main()
     std::cout << "====================================================================================\n";
     std::cout << "Welcome to CSC511C Command Line!\n";
     std::cout << "Type '\033[33mexit\033[0m' to quit, '\033[33mclear\033[0m' to clear the screen, '\033[33mhelp\033[0m' to display other commands.\n\n";
-
-    // Create command dispatcher object
-    CommandDispatcher dispatcher;
-	// Initialize the dispatcher with available commands
-	CommandDispatcher::Initialize(dispatcher);
-
-    bool running = true;
-	// Main command loop
-    while (running) {
-		// Prompt the user for input
-        std::cout << "Enter a command: ";
-        std::string command;
-        std::getline(std::cin, command);
-
-		// Dispatch the command and update the running state based on the command's return value
-		running = dispatcher.DispatchCommand(command);
-    }
-    
-    return 0;
 }
-
