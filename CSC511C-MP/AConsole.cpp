183d735fe0837841e314d2d402497218c0d27e60
jvrosario <julius@restoplus.com.au>
2026-06-24 08:28:55 +0800

    Clean up

--- CSC511C-MP/AConsole.cpp
+++ CSC511C-MP/AConsole.cpp
@@ -1,9 +1,5 @@
 #include "AConsole.h"
 
-
-// 4. [JV] Remove autogeneration of process on start -> Scheduler-Start+Scheduler-Stop, 
-// CPU Ticker, Custom Thread, CPU Worker, config.txt, Process Manager
-
 AConsole::AConsole(const std::string processName) : name(processName) {
 	commandDispatcher = std::make_unique<CommandDispatcher>();
 	commandDispatcher->Initialize();
