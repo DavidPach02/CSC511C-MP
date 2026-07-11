c654f1061da6a62246d868d226485ef14b401488
DLSU-Garcia <andre_emmanuel_garcia@dlsu.edu.ph>
2026-07-11 16:41:43 +0800

    Implemented fix for incorrect filename for memory logger

--- CSC511C-MP/CPUTicker.cpp
+++ CSC511C-MP/CPUTicker.cpp
@@ -101,7 +101,8 @@ void CPUTicker::Run() {
 
 		const uint64_t tick = currentTick.load();
 		if (quantumCycles > 0 && tick > 0 && tick % static_cast<uint64_t>(quantumCycles) == 0) {
-			MemoryLogger::LogTickSnapshot(tick);
+			const uint64_t quantumCycle = tick / static_cast<uint64_t>(quantumCycles);
+			MemoryLogger::LogTickSnapshot(quantumCycle);
 		}
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(tickerDelayMs));
